513f81e7e23e01787de4b7f6ed294fc7e0ba5f30
Haid Ali <70151023@student.uol.edu.pk>
2026-05-24 01:36:35 +0500

    Final project update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBjiPUro0Si3xX4bS_00M6BPHudI40KPjM",
   authDomain: "book-library-4004.firebaseapp.com",
@@ -12,6 +11,7 @@ const firebaseConfig = {
   appId: "1:322530109861:web:2eb9e5c6b6806f1140a04c",
   measurementId: "G-8G269DHTJF"
 };
+
 const app = initializeApp(firebaseConfig);
-export const db   = getFirestore(app);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
